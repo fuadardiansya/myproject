2e426a6bc03cb9cc40f22623a005f3f989398822
fuadardiansya <70194081+fuadardiansya@users.noreply.github.com>
2024-01-16 09:07:32 +0700

    Update text.py

--- streamlit/text.py
+++ streamlit/text.py
@@ -1,1 +1,36 @@
+# Import library
+import streamlit as st
+
+# Title
+st.title("Belajar Machine Learning")
+
+# Header
+st.header("Welcome Machine Learning")
+
+# Subheader
+st.subheader("Bismillahirrahmanirrahim")
+
+# Text
+st.text("lorem ipsum prikitiuw ala ala ye ye")
+
+# Markdown
+st.markdown("# Markdown1")
+st.markdown("## Markdown2")
+st.markdown("### Markdown3")
+st.markdown("#### Markdown4")
+
+# Markdown multi line
+
+# Code block
+
+#LaTex
+
+
+
+
+
+
+
+
+
 
