d35b00c2f76207941521f929df5167280795dcca
fuadardiansya <70194081+fuadardiansya@users.noreply.github.com>
2024-01-16 09:34:23 +0700

    Update text.py

--- streamlit/text.py
+++ streamlit/text.py
@@ -42,7 +42,11 @@ code = '''def hello():
 st.code(code, language='python')
 
 #LaTex
-
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
 
 
 
